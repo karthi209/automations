f2bacfe31d4a1ef28087ef6ac16cb0162c051ff3
karthik <karthikeyan14june@gmail.com>
2024-12-27 10:37:23 +0530

    Update install-firefox.sh

--- install-firefox.sh
+++ install-firefox.sh
@@ -1,17 +1,23 @@
-#! bash
+#!/bin/bash 
 
+# create a directory to store APT repository keys if it doesn't exist
 sudo install -d -m 0755 /etc/apt/keyrings
 
+sudo apt-get install wget
+
+# import the Mozilla APT repository signing key
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 
 sudo apt-get install wget
 
+# add the Mozilla APT repository to your sources list
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 
+# configure APT to prioritize packages from the Mozilla repository
 echo '
 Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
-
+# update package list, and install the Firefox .deb package
 sudo apt-get update && sudo apt-get install firefox
